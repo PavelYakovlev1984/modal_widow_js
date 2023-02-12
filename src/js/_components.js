a6dcf298e3b241f8f263f924563d36cd78a0ace2
Pavel <55947748+PavelYakovlev1984@users.noreply.github.com>
2023-02-12 23:01:19 +0300

    modal end

--- src/js/_components.js
+++ src/js/_components.js
@@ -1,17 +1,25 @@
 'use strict';
-// console.log('components');
 
 let modalWindow = document.querySelector('.modal-window');
 let overlay = document.querySelector('.overlay');
 let closeModalWindow = document.querySelector('.close-modal-window');
 let showModalWindow = document.querySelectorAll('.show-modal-window');
-for (let i = 0; i < showModalWindow.length; i++) {
-  showModalWindow[i].addEventListener('click', function() {
+let removeHiddenClass = function() {
   modalWindow.classList.remove('hidden');
-  overlay.classList.remove('hidden');
-  closeModalWindow.addEventListener('click', function() {
-    modalWindow.classList.add('hidden');
-  overlay.classList.add('hidden');
-  })
-  });
+  overlay.classList.remove('hidden');}
+let addHiddenClass = function() {
+modalWindow.classList.add('hidden');
+overlay.classList.add('hidden');}
+for (let i = 0; i < showModalWindow.length; i++) {
+  showModalWindow[i].addEventListener('click', removeHiddenClass);
 }
+closeModalWindow.addEventListener('click', addHiddenClass);
+overlay.addEventListener('click', addHiddenClass);
+
+document.addEventListener('keydown', function(e){
+  // console.log(e.key);
+  if(e.key == 'Escape') {
+    if(!modalWindow.classList.contains('hidden')){
+      addHiddenClass();
+      console.log(addHiddenClass());
+    }}});
